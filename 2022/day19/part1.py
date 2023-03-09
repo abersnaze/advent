e960cba04d42289027ffb27b6928b7973e2f9ada
George Campbell <gscampbell@netflix.com>
2023-03-08 21:10:00 -0800

    day25 and redo of day19

--- 2022/day19/part1.py
+++ 2022/day19/part1.py
@@ -1,28 +1,15 @@
-#!python3
+#!pypy
 
 import fileinput
 import re
 from collections import defaultdict, Counter
-from itertools import islice, product, repeat
+from itertools import islice, product, repeat, chain
 from functools import reduce
 
+
+LIMIT = 25
 lines = list(map(lambda x: x.strip(), fileinput.input()))
-inventory = Counter(
-    {
-        "ore": 0,
-        "clay": 0,
-        "geode": 0,
-        "obsidian": 0,
-    }
-)
-robots = Counter(
-    {
-        "ore": 1,
-        "clay": 0,
-        "geode": 0,
-        "obsidian": 0,
-    }
-)
+kinds = ["ore", "clay", "obsidian", "geode"]
 blueprint_num = 1
 all_blueprints = defaultdict(list)
 for line in lines:
@@ -39,43 +26,73 @@ for line in lines:
                 amount2,
                 thing2,
             ) = recipe.split(" ")
-            all_blueprints[blueprint_num].append(({"ore": -4}, "ore"))
-            all_blueprints[blueprint_num].append(
-                ({thing1: -int(amount1), thing2: -int(amount2)}, kind)
-            )
+            cost = [0, 0, 0]
+            cost[kinds.index(thing1)] = int(amount1)
+            cost[kinds.index(thing2)] = int(amount2)
+            result = [0, 0, 0, 0]
+            result[kinds.index(kind)] = 1
+            all_blueprints[blueprint_num].append((cost, result))
         else:
             kind, _, _, amount1, thing1 = recipe.split(" ")
-            all_blueprints[blueprint_num].append(({thing1: -int(amount1)}, kind))
+            cost = [0, 0, 0]
+            cost[kinds.index(thing1)] = int(amount1)
+            result = [0, 0, 0, 0]
+            result[kinds.index(kind)] = 1
+            all_blueprints[blueprint_num].append((cost, result))
     blueprint_num += 1
 
 
-def optimal(minute, blueprints, inventory, robots):
-    if minute > 24:
-        return inventory["geode"]
-    # start with just wait for the robots to produce
-    curr_best = 0
-    for cost, robot in blueprints:
-        next_inv = Counter(inventory)
-        next_inv.update(cost)
-        # blueprint costs too much
-        if any(map(lambda v: v < 0, next_inv.values())):
+def mul(foos, x):
+    return (foo * x for foo in foos)
+
+
+def add(foos, bars):
+    return (foo + bar for foo, bar in zip(foos, bars))
+
+
+def sub(foos, bars):
+    return (foo - bar for foo, bar in zip(foos, bars))
+
+
+def use_blueprint(time, robots, inventory, geode_total, cost, result):
+    longest = 0
+    for robot, inv, amount in zip(robots, inventory, cost):
+        if inv >= amount:
             continue
-        # current robots produce ore
-        next_inv.update(robots)
-        # new robot comes online
-        next_robots = Counter(robots)
-        next_robots[robot] = 1
-        best_with = optimal(minute + 1, blueprints, next_inv, next_robots)
-        if curr_best < best_with:
-            curr_best = best_with
-    if curr_best == 0:
-        # current robots produce ore
-        next_inv = Counter(inventory)
-        next_inv.update(robots)
-        curr_best = optimal(minute + 1, blueprints, next_inv, robots)
-    return curr_best
+        if robot == 0:
+            # no amount of time will get what we need
+            return
+        # divide round up
+        duration = (amount - inv + robot - 1) // robot
+        if duration > longest:
+            longest = duration
+    if longest + time < LIMIT - 1:
+        time += longest + 1
+        geode_total += result[3] * (LIMIT - time)
+        inventory = tuple(sub(add(inventory, mul(robots, longest + 1)), cost))
+        robots = tuple(add(robots, result))
+        # state at the end of the time step
+        yield (time, robots, inventory, geode_total)
+
+
+def optimal(blueprints):
+    queue = [(0, (1, 0, 0), (0, 0, 0), 0)]
+    best_at = [0 for t in range(LIMIT)]
+    while queue:
+        state = queue.pop()
+        geodes = state[3]
+        if geodes < best_at[state[0]]:
+            continue
+        if geodes > best_at[state[0]]:
+            for t in range(state[0], LIMIT):
+                best_at[t] == geodes
+        best_at[state[0]] = state[3]
+        for choice in blueprints:
+            queue.extend(use_blueprint(*state, *choice))
+        queue.sort(key=lambda s: s[0], reverse=True)
+    return best_at[LIMIT -1]
 
 
 for num, blueprints in all_blueprints.items():
-    best = optimal(1, blueprints, Counter(inventory), Counter(robots))
+    best = optimal(blueprints)
     print(num, best)
